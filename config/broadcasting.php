2299b78f1aa1c6edea5564f070c0f9cf417802a6
berlim <btlim@up.edu.ph>
2022-05-03 23:39:48 +0800

    update websocket and broadcasting

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -37,11 +37,11 @@ return [
             'app_id' => env('PUSHER_APP_ID'),
             'options' => [
                 'cluster' => env('PUSHER_APP_CLUSTER'),
-                // 'useTLS' => true,
+                'useTLS' => true,
                 'encrypted' => true,
-                'host' => '127.0.0.1',
+                'host' => 'reach-demo.ml',
                 'port' => 6001,
-                'scheme' => 'http'
+                'scheme' => 'https'
             ],
             // 'client_options' => [
             //     // Guzzle client options: https://docs.guzzlephp.org/en/stable/request-options.html
